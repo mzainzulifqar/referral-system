d5a83f9b6cc179c21a72a9768b441bb372a02951
Zain Zulifqar <zainzulifqar21@gmail.com>
2019-09-12 13:39:15 +0500

    final commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+
+        if(isset($data['referral_id']))
+        {
+            $referral = User::where('referral_id',$data['referral_id'])->first();
+                
+                if($referral)
+                {
+                    $referral->balance = $referral->balance + 10;
+                    $referral->update();
+                }
+
+        }
+
+        $user =  User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'referral_id' => uniqid('ref'),
+            'referred_by' => $referral->id ?? 0,
+            'password' => Hash::make($data['password']),
+        ]);
+
+        return $user;
+
+        
+    }
+
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><h2>Users referred by you</h2></div>
+                <div class="card-body">
+                   @if(isset(Auth::user()->referrals))
+               <table class="table table-striped ">
+                   <thead>
+                       <tr>
+                           <th>Name</th>
+                           <th>Email</th>
+                       </tr>
+                   </thead>
+                   <tbody>
+                    @foreach (Auth::user()->referrals as $rf)
+                       <tr>
+                           <td>{{$rf->name}}</td>
+                           <td>{{$rf->email}}</td>
+                       </tr>
+                       @endforeach
+                   </tbody>
+               </table>
+               @else
+               <h2 class="text-center">No referred User yet</h2>
+               @endif
+
+                   </div>
+                </div>
+                </div>
+                <div class="col-md-4">
+                    
+                
+            <div class="card">
+                <div class="card-header"> <h3>Your Referral Address is:</h3></div>
+
+                <div class="card-body">
+
+                   
+                    <span>{{route('register',['referral_id' => Auth::user()->referral_id])}}</span>
+                </div>
+            </div>
+            <div class="card" style="margin-top: 10px;">
+                <div class="card-header"> <h3>Your Referral Balance is:</h3></div>
+
+                <div class="card-body">
+
+                   
+                    <span>{{Auth::user()->balance}}</span>
+                </div>
+            </div>
+        
+
+           </div>
+        </div>
+        </div>
+    </div>
+</div>
+@endsection
